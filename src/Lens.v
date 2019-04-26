5729e9bc6730878ce4cdb6ad324c70f40367cf3b
Tej Chajed <tchajed@mit.edu>
2019-04-26 09:53:36 -0400

    Extend Deterministic with zoom
    Turns out to require some correctness properties for proj and inj (at
    least when written the natural way), so added a mini-lens library to
    bundle well-formedness of proj/inj.

---
+++ src/Lens.v
@@ -0,0 +1,23 @@
+From RecordUpdate Require Import RecordUpdate.
+
+Set Implicit Arguments.
+Generalizable All Variables.
+
+Class Lens X A :=
+  { proj: X -> A;
+    inj: A -> X -> X; }.
+
+Class LensWf X A (lens: Lens X A) :=
+  { inj_proj: forall x, inj (proj x) x = x;
+    proj_inj: forall a x, proj (inj a x) = a; }.
+
+Instance Setter_Lens `(proj: X -> A) (setter:SetterWf proj) : Lens X A :=
+  Build_Lens proj (fun a => set proj (fun _ => a)).
+
+Instance Setter_LensWf `(proj: X -> A) (setter_wf: SetterWf proj)
+  : LensWf (Setter_Lens setter_wf).
+Proof.
+  unfold Setter_Lens; constructor; simpl; intros.
+  rewrite set_eq; auto.
+  rewrite set_get; auto.
+Qed.
